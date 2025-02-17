39c4d97028916657887a9fd29569b1146b114a9d
DESKTOP-KMNUJB9\dkemfdk <dkemfdk4@Naver.com>
2025-02-17 23:10:07 +0900

    post vs get

--- Thx_boardService-pratice/src/main/java/com/thxforservice/board/controllers/CommentController.java
+++ Thx_boardService-pratice/src/main/java/com/thxforservice/board/controllers/CommentController.java
@@ -1,24 +1,3 @@
-package com.thxforservice.board.controllers;
-
-import com.thxforservice.board.entities.CommentData;
-import com.thxforservice.board.services.comment.CommentDeleteService;
-import com.thxforservice.board.services.comment.CommentInfoService;
-import com.thxforservice.board.services.comment.CommentSaveService;
-import com.thxforservice.board.validators.CommentValidator;
-import com.thxforservice.global.Utils;
-import com.thxforservice.global.exceptions.BadRequestException;
-import com.thxforservice.global.rests.JSONData;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.Parameters;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 @Tag(name="Comment", description = "댓글 API")
 @RestController
 @RequestMapping("/comment")
@@ -30,6 +9,11 @@ public class CommentController {
     private final CommentValidator validator;
     private final Utils utils;
 
+    /**
+     * 댓글 작성 API
+     * - HTTP Method: POST
+     * - 요청 본문(@RequestBody)으로 데이터를 전달하여 새로운 댓글을 작성함
+     */
     @Operation(summary = "댓글 작성", method = "POST")
     @ApiResponse(responseCode = "200")
     @Parameters({
@@ -43,6 +27,11 @@ public class CommentController {
         return save(form, errors);
     }
 
+    /**
+     * 댓글 수정 API
+     * - HTTP Method: PATCH
+     * - 기존 댓글 데이터를 수정함
+     */
     @Operation(summary = "댓글 수정", method = "PATCH")
     @ApiResponse(responseCode = "200")
     @Parameters({
@@ -50,11 +39,15 @@ public class CommentController {
             @Parameter(name="seq", required = true, description = "댓글 등록번호", example = "100"),
             @Parameter(name="commenter", required = true, description = "작성자", example = "작성자01"),
             @Parameter(name="content", required = true, description = "댓글 내용")
-    })   @PatchMapping
+    })
+    @PatchMapping
     public JSONData update(@RequestBody @Valid RequestComment form, Errors errors) {
         return save(form, errors);
     }
 
+    /**
+     * 댓글 저장 로직 (작성 및 수정 공통)
+     */
     public JSONData save(RequestComment form, Errors errors) {
         validator.validate(form, errors);
 
@@ -69,35 +62,46 @@ public class CommentController {
         return new JSONData(items);
     }
 
+    /**
+     * 특정 댓글 조회 API
+     * - HTTP Method: GET
+     * - 경로 변수(@PathVariable)로 댓글 번호(seq)를 받아 해당 댓글 정보를 조회함
+     */
     @Operation(summary = "댓글 하나 조회", description = "댓글 번호를 가지고 작성된 댓글을 조회 한다.", method = "GET")
     @ApiResponse(responseCode = "200")
     @Parameter(name="seq", required = true, description = "경로변수, 댓글 등록 번호")
     @GetMapping("/info/{seq}")
     public JSONData getInfo(@PathVariable("seq") Long seq) {
         CommentData item = infoService.get(seq);
-
         return new JSONData(item);
     }
 
+    /**
+     * 특정 게시글의 모든 댓글 조회 API
+     * - HTTP Method: GET
+     * - 게시글 번호(bSeq)를 받아 해당 게시글의 모든 댓글을 조회함
+     */
     @Operation(summary = "댓글 조회", description = "게시글 번호를 가지고 작성된 댓글 목록을 조회 한다.", method = "GET")
     @ApiResponse(responseCode = "200")
     @Parameter(name="bSeq", required = true, description = "경로변수, 게시글 번호")
     @GetMapping("/list/{bSeq}")
     public JSONData getList(@PathVariable("bSeq") Long bSeq) {
         List<CommentData> items = infoService.getList(bSeq);
-
         return new JSONData(items);
     }
 
+    /**
+     * 댓글 삭제 API
+     * - HTTP Method: DELETE
+     * - 댓글 번호(seq)를 받아 해당 댓글을 삭제함
+     */
     @Operation(summary = "댓글 하나 삭제", description = "댓글 번호를 가지고 작성된 댓글을 삭제한다.", method = "DELETE")
     @ApiResponse(responseCode = "200")
     @Parameter(name="seq", required = true, description = "경로변수, 댓글 등록 번호")
     @DeleteMapping("/{seq}")
     public JSONData delete(@PathVariable("seq") Long seq) {
         Long bSeq = deleteService.delete(seq);
-
         List<CommentData> items = infoService.getList(bSeq);
-
         return new JSONData(items);
     }
 }
