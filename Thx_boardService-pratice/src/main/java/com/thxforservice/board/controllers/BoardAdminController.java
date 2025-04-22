ebc73c0cd3e85aa8a382fe6020d21c6bb4e67492
DESKTOP-KMNUJB9\dkemfdk <dkemfdk4@Naver.com>
2025-04-22 20:24:33 +0900

    주석

--- Thx_boardService-pratice/src/main/java/com/thxforservice/board/controllers/BoardAdminController.java
+++ Thx_boardService-pratice/src/main/java/com/thxforservice/board/controllers/BoardAdminController.java
@@ -22,7 +22,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name="BoardAdmin", description = "게시판 관리")
+@Tag(name = "BoardAdmin", description = "게시판 관리")
 @RestController
 @RequestMapping("/admin")
 @RequiredArgsConstructor
@@ -35,17 +35,18 @@ public class BoardAdminController {
     private final HttpServletRequest request;
     private final Utils utils;
 
+    // 게시판 등록 or 수정
     @Operation(summary = "게시판 등록")
     @ApiResponse(responseCode = "201", description = "게시판 등록 성공시 201")
     @Parameters({
-            @Parameter(name="mode", required = true, description = "add - 등록, edit - 수정"),
-            @Parameter(name="gid", required = true, description = "그룹 ID, 게시판 상단, 하단 이미지 관련"),
-            @Parameter(name="listOrder", description = "진열 가중치, 수치가 높을 수록 먼저 게시판 노출"),
-            @Parameter(name="bid", required = true, description = "게시판 아이디"),
-            @Parameter(name="bname", required = true, description = "게시판 이름"),
-            @Parameter(name="active", example = "false", description = "사용여부")
+            @Parameter(name = "mode", required = true, description = "add - 등록, edit - 수정"),
+            @Parameter(name = "gid", required = true, description = "그룹 ID, 게시판 상단, 하단 이미지 관련"),
+            @Parameter(name = "listOrder", description = "진열 가중치, 수치가 높을 수록 먼저 게시판 노출"),
+            @Parameter(name = "bid", required = true, description = "게시판 아이디"),
+            @Parameter(name = "bname", required = true, description = "게시판 이름"),
+            @Parameter(name = "active", example = "false", description = "사용여부")
     })
-    @RequestMapping(method={RequestMethod.POST, RequestMethod.PATCH}, path="/save")
+    @RequestMapping(method = {RequestMethod.POST, RequestMethod.PATCH}, path = "/save")
     public ResponseEntity<Void> save(@RequestBody @Valid RequestBoardConfig form, Errors errors) {
 
         String method = request.getMethod().toUpperCase();
@@ -63,47 +64,42 @@ public class BoardAdminController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    // 게시판 설정 조회 (단건)
     @Operation(summary = "게시판 설정 하나 조회", description = "설정 조회는 게시판 아이디(bid) 기준 입니다.")
     @ApiResponse(responseCode = "200")
-    @Parameter(name="bid", required = true, description = "경로변수", example = "notice")
+    @Parameter(name = "bid", required = true, description = "경로변수", example = "notice")
     @GetMapping("/info/{bid}")
     public JSONData info(@PathVariable("bid") String bid) {
         Board board = configInfoService.get(bid);
-
         return new JSONData(board);
     }
 
-
+    // 게시판 설정 목록 조회
     @Operation(summary = "게시판 목록 조회")
     @ApiResponse(responseCode = "200", description = "items - 조회된 게시글 목록, pagination - 페이징 기초 데이터")
     @Parameters({
-            @Parameter(name="page", description = "페이지 번호", example = "1"),
-            @Parameter(name="limit", description = "한페이지당 레코드 갯수", example = "20"),
-            @Parameter(name="sopt", description = "검색옵션", example = "ALL"),
-            @Parameter(name="skey", description = "검색키워드"),
-            @Parameter(name="bid", description = "게시판 ID"),
-            @Parameter(name="bids", description = "게시판 ID 목록"),
-            @Parameter(name="bname", description = "게시판 이름"),
-            @Parameter(name="active", description = "게시판 사용중 여부", example = "true")
+            @Parameter(name = "page", description = "페이지 번호", example = "1"),
+            @Parameter(name = "limit", description = "한페이지당 레코드 갯수", example = "20"),
+            @Parameter(name = "sopt", description = "검색옵션", example = "ALL"),
+            @Parameter(name = "skey", description = "검색키워드"),
+            @Parameter(name = "bid", description = "게시판 ID"),
+            @Parameter(name = "bids", description = "게시판 ID 목록"),
+            @Parameter(name = "bname", description = "게시판 이름"),
+            @Parameter(name = "active", description = "게시판 사용중 여부", example = "true")
     })
     @GetMapping("/list")
     public JSONData list(@ModelAttribute BoardSearch search) {
-
         ListData data = configInfoService.getList(search, true);
         return new JSONData(data);
     }
 
-
-
-
+    // 게시판 삭제
     @Operation(summary = "게시판 삭제", description = "게시판 아이디(bid) 기준으로 게시판을 삭제합니다.")
     @ApiResponse(responseCode = "200", description = "게시판 삭제 성공")
     @ApiResponse(responseCode = "404", description = "존재하지 않는 게시판")
     @Parameter(name = "bid", required = true, description = "게시판 아이디")
     @DeleteMapping("/delete/{bid}")
-    public void  deleteBoard(@PathVariable("bid") String bid) {
+    public void deleteBoard(@PathVariable("bid") String bid) {
         boardConfigDeleteService.delete(bid);
     }
-
-
 }
